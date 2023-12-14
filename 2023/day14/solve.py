0f2ab685e67d116db81191705c65dbeb20b54c97
Chad Russell <chaddouglasrussell@gmail.com>
2023-12-14 17:15:03 -0500

    working day 14

--- 2023/day14/solve.py
+++ 2023/day14/solve.py
@@ -1,17 +1,18 @@
-input = list(map(list, open(0).read().splitlines()))
-
-nrows = len(input)
-ncols = len(input[0])
+input = open(0).read().splitlines()
+input = [list(row) for row in input]
 
 def move_rock(src_ri, src_ci, dst_ri, dst_ci):
     input[dst_ri][dst_ci] = 'O'
     input[src_ri][src_ci] = '.'
 
 def load_factor(ri):
-    return nrows - ri
+    return len(input) - ri
 
 def num_rocks(row):
     return sum(ch == 'O' for ch in row)
+
+def calculate_load():
+    return sum(load_factor(ri) * num_rocks(row) for ri, row in enumerate(input))
 
 def roll_north(ri, ci):
     if input[ri][ci] != 'O':
@@ -21,86 +22,51 @@ def roll_north(ri, ci):
     if stopping_point != ri:
         move_rock(ri, ci, stopping_point, ci)
 
-def roll_south(ri, ci):
-    if input[ri][ci] != 'O':
-        return
-    stopping_point = (n for n in range(ri + 1, nrows, 1) if input[n][ci] != '.')
-    stopping_point = next(stopping_point, nrows) - 1
-    if stopping_point != ri:
-        move_rock(ri, ci, stopping_point, ci)
+def roll_all_north():
+    for ri in range(len(input)):
+        for ci in range(len(input[0])):
+            roll_north(ri, ci)
 
-def roll_west(ri, ci):
-    if input[ri][ci] != 'O':
-        return
-    stopping_point = (n for n in range(ci - 1, -1, -1) if input[ri][n] != '.')
-    stopping_point = next(stopping_point, -1) + 1
-    if stopping_point != ci:
-        move_rock(ri, ci, ri, stopping_point)
+def rotate():
+    global input
+    input = [''.join(row) for row in input]
+    input = list(zip(*input[::-1]))
+    input = [list(row) for row in input]
 
-def roll_east(ri, ci):
-    if input[ri][ci] != 'O':
-        return
-    stopping_point = (n for n in range(ci + 1, ncols, 1) if input[ri][n] != '.')
-    stopping_point = next(stopping_point, ncols) - 1
-    if stopping_point != ci:
-        move_rock(ri, ci, ri, stopping_point)
-
-def calculate_load():
-    return sum(load_factor(ri) * num_rocks(row) for ri, row in enumerate(input))
+def spin_cycle():
+    for _ in range(4):
+        roll_all_north()
+        rotate()
 
 def part1():
-    for ri, row in enumerate(input):
-        for ci in range(len(row)):
+    for ri in range(len(input)):
+        for ci in range(len(input[0])):
             roll_north(ri, ci)
 
     answer = calculate_load()
     # assert(load == 113486)
     return answer
 
-def print_input():
-    for row in input:
-        print(''.join(row))
-    print('')
-
-def cycle():
-    for ri in range(nrows):
-        for ci in range(ncols):
-            roll_north(ri, ci)
-
-    for ci in range(ncols):
-        for ri in range(nrows):
-            roll_west(ri, ci)
-
-    for ri in range(nrows - 1, -1, -1):
-        for ci in range(ncols):
-            roll_south(ri, ci)
-
-    for ci in range(ncols - 1, -1, -1):
-        for ri in range(nrows):
-            roll_east(ri, ci)
-
 def part2():
+    nloads = 150
     loads = []
-    for _ in range(500):
-        cycle()
+    for _ in range(nloads):
+        spin_cycle()
         loads.append(calculate_load())
 
-    # hack to try to detect cycle length
-    cycle_len = 0
-    min_cycle_length = 10
-    pat = loads[100:100 + min_cycle_length]
-    for i in range(100 + min_cycle_length + 1, 500 - min_cycle_length):
-        if loads[i:i+min_cycle_length] == pat:
-            cycle_len = i - 100
-            break
+    start = 100
+    min_cycle_len = 10
 
-    remainder = (1_000_000_000 - 100) % cycle_len
+    pat = loads[start:start + min_cycle_len]
+    seek_range = range(start + min_cycle_len + 1, nloads - 1 - min_cycle_len)
 
-    print(f'cycle_len: {cycle_len}')
-    print(f'multiplier: {(1_000_000_000 - 100) // cycle_len}')
-    print(f'remainder: {remainder}')
+    cycle_len = next(i - start for i in seek_range if loads[i:i + min_cycle_len] == pat)
+    remainder = (1_000_000_000 - start) % cycle_len
+    answer = loads[start + remainder - 1]
 
-    return loads[100 + remainder - 1]
+    # assert(answer == 104409)
 
-# print(f'part 1: {part1()}')
+    return answer
+
+print(f'part 1: {part1()}')
 print(f'part 2: {part2()}')
