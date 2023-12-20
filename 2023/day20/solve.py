076605017d0e2bd94e1fc90f96f033f3c5282b97
Chad Russell <chaddouglasrussell@gmail.com>
2023-12-20 13:56:17 -0500

    day 20 cleaner with classes

--- 2023/day20/solve.py
+++ 2023/day20/solve.py
@@ -8,10 +8,21 @@ class Pulse(Enum):
     LOW = 0
     HIGH = 1
 
-class Module(Enum):
+class ModuleType(Enum):
     BROADCASTER = 2
     FLIPFLOP = 3
     CONJUNCTION = 4
+
+class Module:
+    def __init__(self, ty, dst):
+        self.ty = ty
+        self.dst = dst
+        match ty:
+            case ModuleType.FLIPFLOP:
+                self.state = Pulse.LOW
+            case ModuleType.CONJUNCTION:
+                self.input_states = {}
+
 
 modules = {}
 
@@ -21,12 +32,12 @@ for line in input:
     dst = list(dst.split(', '))
 
     if src == 'broadcaster':
-        modules[src] = { 'ty': Module.BROADCASTER, 'dst': dst }
+        modules[src] = Module(ModuleType.BROADCASTER, dst)
     elif src[0] == '%':
-        modules[src[1:]] = { 'ty': Module.FLIPFLOP, 'dst': dst, 'state': Pulse.LOW }
+        modules[src[1:]] = Module(ModuleType.FLIPFLOP, dst)
     else:
         assert(src[0] == '&')
-        modules[src[1:]] = { 'ty': Module.CONJUNCTION, 'dst': dst, 'states': {} }
+        modules[src[1:]] = Module(ModuleType.CONJUNCTION, dst)
 
 leads_to_rx = None
 conjunctions = []
@@ -36,14 +47,15 @@ first_seen_conjunctions = {}
 # Pull out the list of modules which lead to `rx`
 for mk in modules.keys():
     m = modules[mk]
-    if m['ty'] == Module.CONJUNCTION:
-        sources = [smk for smk in modules.keys() if mk in modules[smk]['dst']]
-        m['states'] = {s: Pulse.LOW for s in sources}
-    if 'rx' in m['dst']:
+    if m.ty == ModuleType.CONJUNCTION:
+        sources = [smk for smk in modules.keys() if mk in modules[smk].dst]
+        m.states = {s: Pulse.LOW for s in sources}
+    if 'rx' in m.dst:
         leads_to_rx = mk
 
-# Pull out anything which leads to something in leads_to_rx
-conjunctions = [mk for mk, m in modules.items() if leads_to_rx in m['dst']]
+# Pull out anything which leads to something in leads_to_rx. In this input,
+# they are all conjunction nodes.
+conjunctions = [mk for mk, m in modules.items() if leads_to_rx in m.dst]
 
 high_pulses = 0
 low_pulses = 0
@@ -75,23 +87,22 @@ def push_button():
             continue
 
         module = modules[dst]
-        match module['ty']:
-            case Module.BROADCASTER:
-                for d in module['dst']:
+        match module.ty:
+            case ModuleType.BROADCASTER:
+                for d in module.dst:
                     pulse_list.append((dst, d, pulse))
-            case Module.FLIPFLOP:
+            case ModuleType.FLIPFLOP:
                 if pulse == Pulse.HIGH:
                     continue
-                module['state'] = Pulse.HIGH if module['state'] == Pulse.LOW else Pulse.LOW
-                for d in module['dst']:
-                    pulse_list.append((dst, d, module['state']))
-            case _:
-                assert(module['ty'] == Module.CONJUNCTION)
-                module['states'][src] = pulse
+                module.state = Pulse.HIGH if module.state == Pulse.LOW else Pulse.LOW
+                for d in module.dst:
+                    pulse_list.append((dst, d, module.state))
+            case ModuleType.CONJUNCTION:
+                module.states[src] = pulse
                 new_pulse = Pulse.HIGH
-                if all(a == Pulse.HIGH for a in module['states'].values()):
+                if all(a == Pulse.HIGH for a in module.states.values()):
                     new_pulse = Pulse.LOW
-                for d in module['dst']:
+                for d in module.dst:
                     pulse_list.append((dst, d, new_pulse))
 
 def part1():
