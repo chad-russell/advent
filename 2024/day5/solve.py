e0d32f976e8a3a19d96c9b04dd92507e648adf2a
Chad Russell <chaddouglasrussell@gmail.com>
2024-12-05 14:04:26 -0500

    day 5

--- 2024/day5/solve.py
+++ 2024/day5/solve.py
@@ -1,20 +1,18 @@
-input = open(0).read().splitlines()
-
-newline_index = input.index('')
-orderings = [list(map(int, e.split('|'))) for e in input[:newline_index]]
-updates = [list(map(int, e.split(','))) for e in input[newline_index+1:]]
+orderings, updates = open(0).read().split('\n\n')
+orderings = [list(map(int, line.split('|'))) for line in orderings.splitlines()]
+updates = [list(map(int, line.split(','))) for line in updates.splitlines()]
 
 already_correct = [u for u in updates if all(u.index(o[0]) < u.index(o[1]) for o in orderings if o[0] in u and o[1] in u)]
 
 def part1():
     return sum(u[len(u) // 2] for u in already_correct)
 
-def order_swap(u, i0, i1):
+def order_swap(u, i0, i1) -> bool:
     if i0 < i1: return False
     u[i0], u[i1] = u[i1], u[i0]
     return True
 
-def midpoint_of_ordered(u):
+def midpoint_of_ordered(u) -> int:
     relevant_orderings = [o for o in orderings if o[0] in u and o[1] in u]
     while any([order_swap(u, u.index(o0), u.index(o1)) for o0, o1 in relevant_orderings]): pass
     return u[len(u) // 2]
